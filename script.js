d7fd7541e81705d108da0cd2e251b79b58de9d85
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-12-22 11:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const p=new Promise((res)=>{
+	setTimeout(() => {
+        let array = [1, 2, 3, 4];
+        res(array);
+    }, 3000);
+});
+function resolveArray(){
+	p.then((data)=>{
+		let even=data.filter((num)=>num%2==0);
+		  setTimeout(()=>{
+			  let ele=document.getElementById("output");
+			  ele.innerHTML=even;
+		  },1000);
+		 return even;
+	}).then((even)=>{
+		let double=even.map((i)=>i*2);
+		setTimeout(()=>{
+			  let ele=document.getElementById("output");
+			  ele.innerHTML=double;
+		  },2000);
+		
+	})
+}
+resolveArray();
